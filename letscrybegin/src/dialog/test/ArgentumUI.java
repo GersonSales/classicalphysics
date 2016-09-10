a21004354ea334753c01ac63285773e8d5fbfa2f
Gerson <gerson.junior@ccc.ufcg.edu.br>
2016-09-10 02:49:36 -0300

    adicao de classe inutil

--- letscrybegin/src/dialog/test/ArgentumUI.java
+++ letscrybegin/src/dialog/test/ArgentumUI.java
@@ -1,5 +1,60 @@
 package dialog.test;
 
-public class ArgentumUI {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class ArgentumUI {
+	private static final int HEIGTH = 540;
+	private static final int WIDTH = 540;
+
+	public static void main(String[] args) {
+		new ArgentumUI().montaTela();
+	}
+
+	private JFrame janela;
+	private JPanel painelPrincipal;
+
+	private void montaTela() {
+		preparaJanela();
+		preparaPainelPrincipal();
+		preparaBotaoCarregar();
+		preparaBotaoSair();
+		mostraJanela();
+	}
+
+	private void preparaJanela() {
+		janela = new JFrame("ArgentumUI");
+		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	private void preparaPainelPrincipal() {
+		painelPrincipal = new JPanel();
+		janela.add(painelPrincipal);
+	}
+
+	private void preparaBotaoCarregar() {
+		JButton botaoCarregar = new JButton("Carregar XML");
+		botaoCarregar.addActionListener((event) -> new EscolhedorDeXML().escolhe());
+		painelPrincipal.add(botaoCarregar);
+
+	}
+
+	private void preparaBotaoSair() {
+		JButton botaoSair = new JButton("Sair");
+
+		botaoSair.addActionListener((event) -> System.exit(0));
+
+		painelPrincipal.add(botaoSair);
+
+	}
+
+	private void mostraJanela() {
+		janela.pack();
+		janela.setSize(WIDTH, HEIGTH);
+		janela.setVisible(true);
+	}
 }
---
+++ letscrybegin/src/dialog/test/EscolhedorDeXML.java
@@ -0,0 +1,10 @@
+package dialog.test;
+
+public class EscolhedorDeXML {
+
+	public void escolhe() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
